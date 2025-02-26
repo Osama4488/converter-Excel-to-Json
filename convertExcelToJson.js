3a08a5774c8dbd1815dfc704d9d7e651dd8bee7f
Syed Osama ALi <syed.osama.ali.96@gmail.com>
2025-02-26 18:54:34 +0500

    Add files via upload

---
+++ convertExcelToJson.js
@@ -0,0 +1,22 @@
+const xlsx = require("xlsx");
+const fs = require("fs");
+
+// Load the Excel file
+const workbook = xlsx.readFile("data.xlsx");
+
+
+
+
+// Get the first sheet name
+const sheetName = workbook.SheetNames[0];
+
+
+console.log(sheetName,"sheetName")
+
+// Convert the sheet data to JSON
+const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+// Save JSON to a file
+fs.writeFileSync("data.json", JSON.stringify(jsonData, null, 2));
+
+console.log("Excel data has been converted to JSON and saved as 'data.json'.");
