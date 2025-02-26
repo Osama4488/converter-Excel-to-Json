116543c0e56aa5c97741e1c68b84c3ef269e4387
Osama dev <syed.osama@logicielservice.com>
2025-02-26 18:32:32 +0500

    changed a little for console log

--- convertExcelToJson.js
+++ convertExcelToJson.js
@@ -4,8 +4,14 @@ const fs = require("fs");
 // Load the Excel file
 const workbook = xlsx.readFile("data.xlsx");
 
+
+
+
 // Get the first sheet name
 const sheetName = workbook.SheetNames[0];
+
+
+console.log(sheetName,"sheetName")
 
 // Convert the sheet data to JSON
 const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
